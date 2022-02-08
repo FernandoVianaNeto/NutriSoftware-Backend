4f44f172730996fe6f750e5fa85c64f2d58585c1
Fernando <fernando.viana.nt@gmail.com>
2022-02-08 08:52:35 -0300

    AuthController

--- src/app/controllers/AuthController.ts
+++ src/app/controllers/AuthController.ts
@@ -1,5 +1,31 @@
-class AuthController {
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserRepository = require('../repositories/UserRepository.ts');
+const authConfig = require('../../config/authConfig.ts');
 
+class AuthController {
+  async login(request: any, response: any) {
+    const { email, password } = request.body;
+
+    const user = await UserRepository.findUserByEmail(email);
+
+    if (!user) {
+      return response.sendStatus(401).json({ error: 'User not found' });
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      return response.sendStatus(401).json({ error: 'Invalid password' });
+    }
+
+    const { id } = user;
+
+    return response.json({
+      user,
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: 84600,
+      }),
+    });
+  }
 }
 
 module.exports = AuthController;
