454e906825e71e9c3255fa93eac9d51945677e67
Fernando <fernando.viana.nt@gmail.com>
2022-02-08 08:45:40 -0300

    gitignore

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 const Router = require('express');
 
+const authMiddleware = require('./app/middlewares/authMiddleware.ts');
+
 const UserController = require('./app/controllers/UserController.ts');
 
 const router = Router();
@@ -7,5 +9,11 @@ const router = Router();
 router.get('/users', UserController.index);
 router.post('/createuser', UserController.create);
 router.post('/deleteuser/:id', UserController.delete);
+
+// Inicio das rotas protegidas por autenticação
+
+router.use(authMiddleware);
+
+// Rotas protegidas
 
 module.exports = router;
